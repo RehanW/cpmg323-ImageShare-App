4f7d555a3bff088223f18a72ddd86f65adbddce8
Rehan Willemse <rehan.willemse@gmail.com>
2021-11-20 11:19:50 +0200

    early changes and setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Login from "./Pages/Login"
+import Signup from "./Pages/Signup"
+import Home from "./Pages/Home"
+import Images from "./Pages/Images"
 
 function App() {
   return (
     <div className="App">
+      <Login />
+      <Signup />
+      <Home />
+      <Images />
     </div>
   );
 }
